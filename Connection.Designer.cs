8540311f2d1b8727211a41a3f88b0811d952ddf7
MaatheN <contact.pro@mathieu-neveu.fr>
2019-12-16 11:41:34 +0100

    Suppression btn connec

--- Connection.Designer.cs
+++ Connection.Designer.cs
@@ -34,7 +34,6 @@
 			this.label1 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
 			this.connect = new System.Windows.Forms.Button();
-			this.button1 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// login
@@ -43,10 +42,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.login.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.login.Location = new System.Drawing.Point(179, 174);
-			this.login.Margin = new System.Windows.Forms.Padding(4);
+			this.login.Location = new System.Drawing.Point(268, 272);
+			this.login.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
 			this.login.Name = "login";
-			this.login.Size = new System.Drawing.Size(165, 45);
+			this.login.Size = new System.Drawing.Size(246, 68);
 			this.login.TabIndex = 0;
 			// 
 			// password
@@ -55,10 +54,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.password.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.password.Location = new System.Drawing.Point(179, 292);
-			this.password.Margin = new System.Windows.Forms.Padding(4);
+			this.password.Location = new System.Drawing.Point(268, 456);
+			this.password.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
 			this.password.Name = "password";
-			this.password.Size = new System.Drawing.Size(165, 45);
+			this.password.Size = new System.Drawing.Size(246, 68);
 			this.password.TabIndex = 1;
 			// 
 			// label1
@@ -69,10 +68,10 @@
 			this.label1.AutoSize = true;
 			this.label1.BackColor = System.Drawing.Color.Transparent;
 			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(183, 131);
-			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Location = new System.Drawing.Point(274, 205);
+			this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(101, 39);
+			this.label1.Size = new System.Drawing.Size(160, 63);
 			this.label1.TabIndex = 2;
 			this.label1.Text = "Login";
 			this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -85,10 +84,10 @@
 			this.label2.AutoSize = true;
 			this.label2.BackColor = System.Drawing.Color.Transparent;
 			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label2.Location = new System.Drawing.Point(171, 250);
-			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Location = new System.Drawing.Point(256, 391);
+			this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(220, 39);
+			this.label2.Size = new System.Drawing.Size(349, 63);
 			this.label2.TabIndex = 3;
 			this.label2.Text = "Mot de passe";
 			this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -97,38 +96,27 @@
 			// 
 			this.connect.Anchor = System.Windows.Forms.AnchorStyles.None;
 			this.connect.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.connect.Location = new System.Drawing.Point(114, 398);
-			this.connect.Margin = new System.Windows.Forms.Padding(4);
+			this.connect.Location = new System.Drawing.Point(171, 622);
+			this.connect.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
 			this.connect.Name = "connect";
-			this.connect.Size = new System.Drawing.Size(310, 59);
+			this.connect.Size = new System.Drawing.Size(465, 92);
 			this.connect.TabIndex = 4;
 			this.connect.Text = "Se connecter";
 			this.connect.UseVisualStyleBackColor = true;
 			this.connect.Click += new System.EventHandler(this.button1_Click);
 			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(606, 352);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(131, 105);
-			this.button1.TabIndex = 5;
-			this.button1.Text = "GoTo UserGestion";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click_1);
-			// 
 			// Connection
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-			this.ClientSize = new System.Drawing.Size(805, 547);
-			this.Controls.Add(this.button1);
+			this.ClientSize = new System.Drawing.Size(1208, 855);
 			this.Controls.Add(this.connect);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.password);
 			this.Controls.Add(this.login);
-			this.Margin = new System.Windows.Forms.Padding(4);
+			this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
 			this.Name = "Connection";
 			this.Text = "Connectez-vous";
 			this.Load += new System.EventHandler(this.Form1_Load);
@@ -144,7 +132,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button connect;
-		private System.Windows.Forms.Button button1;
 	}
 }
 
