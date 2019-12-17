c4ec3177521b1b1971bc1f9b9fe50c659934a528
matthieumenard37 <matthieu.menard@viacesi.fr>
2019-12-17 11:37:07 +0100

    creation of classes "article" and "family"

---
+++ Classes/Article.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestinv.Classes
+{
+    public class Article
+    {
+
+    }
+}
+
+// COTE ADMIN
+// Visualiser articles
+// Ajouter articles
+// Retirer articles
+// Modifier articles
---
+++ Classes/Family.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestinv.Classes
+{
+    public class Family
+    {
+    }
+}
+
+// COTE ADMIN
+// Visualiser familles
+// Ajouter familles
+// Retirer familles
+// Modifier familles
